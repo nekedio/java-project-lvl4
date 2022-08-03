69eb1d3f079741a8e22016d98ca33e0fcb26fc1a
nekedio <nikitasi@tatmedia.com>
2022-08-03 09:28:00 +0300

    connected javalin

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,30 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.valueOf(port);
+    }
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.enableDevLogging();
+        });
+
+        addRoutes(app);
+
+        return app;
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
