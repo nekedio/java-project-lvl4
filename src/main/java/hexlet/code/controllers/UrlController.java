f0599b15641a16f2d202513f9631a834dc11372e
nekedio <nikitasi@tatmedia.com>
2022-08-11 16:28:35 +0300

    added validation and changed views

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -2,6 +2,7 @@ package hexlet.code.controllers;
 
 import hexlet.code.models.Url;
 import hexlet.code.models.query.QUrl;
+import hexlet.code.services.UrlService;
 import io.javalin.core.validation.JavalinValidation;
 import io.javalin.core.validation.ValidationError;
 import io.javalin.core.validation.Validator;
@@ -14,11 +15,11 @@ public final class UrlController {
     public static Handler createUrl = ctx -> {
         String line = ctx.formParam("url");
 
-        Validator<String> urlValidator = ctx.formParamAsClass("url", String.class)
-                .check(l -> !l.isEmpty(), "Пожалуйста, введите корректный адрес сайта!");
+        Validator<String> validator = ctx.formParamAsClass("url", String.class)
+                .check(l -> !l.isEmpty(), "Поле не должно быть пустым!");
 
         Map<String, List<ValidationError<? extends Object>>> errors
-                = JavalinValidation.collectErrors(urlValidator);
+                = JavalinValidation.collectErrors(validator);
 
         if (!errors.isEmpty()) {
             ctx.status(422);
@@ -28,10 +29,30 @@ public final class UrlController {
             return;
         }
 
-        Url url = new Url(line);
+        UrlService urlService = new UrlService(line);
+
+        if (!urlService.isValid()) {
+            ctx.status(422);
+            ctx.sessionAttribute("flashError", "Пожалуйста, введите корректный URL!");
+            ctx.attribute("errors", Map.of());
+            ctx.attribute("url", new Url(line));
+            ctx.render("home.html");
+            return;
+        }
+
+        List<Url> urls = new QUrl().findList();
+        Url url = new Url(urlService.getUrlName());
+
+        if (UrlService.containedIn(urls, url)) {
+            ctx.sessionAttribute("flashInfo", "URL уже добавлен!");
+            ctx.redirect("/urls");
+            return;
+        }
+
         url.save();
-        ctx.sessionAttribute("flash", "Пользователь успешно создан!");
+        ctx.sessionAttribute("flashSuccess", "Пользователь успешно создан!");
         ctx.redirect("/urls");
+        return;
     };
 
     public static Handler list = ctx -> {
--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO url
   (name, created_at)
 VALUES
-  ('google.com', '2020-12-12'),
-  ('yahoo.com', '2020-12-12'),
-  ('yandex.ru', '2020-12-12');
+  ('https://www.google.com', '2020-12-12'),
+  ('https://yandex.ru', '2020-12-12'),
+  ('https://www.yahoo.com', '2020-12-12');
